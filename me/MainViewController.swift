06046fa9241a4e429e0f521da6fc92921eafd4e3
Jamie De Vivo (i7436295) <i7436295@mac42943.student.bournemouth.ac.uk>
2019-02-16 22:47:33 +0000

    Played around with SnapKit
    Created 4 UI SubViews and a Button

---
+++ me/MainViewController.swift
@@ -0,0 +1,63 @@
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subViewOne = UIView()
+        let subViewTwo = UIView()
+        let subViewThree = UIView()
+        let subViewFour = UIView()
+        
+        let button = UIButton()
+        
+        subViewOne.backgroundColor = .red
+        subViewTwo.backgroundColor = .purple
+        subViewThree.backgroundColor = .blue
+        subViewFour.backgroundColor = .green
+        button.backgroundColor = .black
+        button.setTitle("Test Button", for: .normal)
+
+        self.view.addSubview(subViewOne)
+        self.view.addSubview(subViewTwo)
+        self.view.addSubview(subViewThree)
+        self.view.addSubview(subViewFour)
+        self.view.addSubview(button)
+        
+        UIView.animate(withDuration: 1) {
+            button.snp.makeConstraints { (make) in
+                make.size.equalTo(100)
+                make.center.equalTo(self.view)
+            }
+            subViewOne.snp.makeConstraints { (make) in
+                make.size.equalTo(self.view).multipliedBy(0.5)
+                make.left.equalTo(self.view)
+                make.top.equalTo(self.view)
+            }
+            subViewTwo.snp.makeConstraints { (make) in
+                make.size.equalTo(self.view).multipliedBy(0.5)
+                make.bottom.equalTo(self.view)
+                make.left.equalTo(self.view)
+            }
+            subViewThree.snp.makeConstraints { (make) in
+                make.size.equalTo(self.view).multipliedBy(0.5)
+                make.right.equalTo(self.view)
+                make.bottom.equalTo(self.view)
+            }
+            subViewFour.snp.makeConstraints { (make) in
+                make.size.equalTo(self.view).multipliedBy(0.5)
+                make.right.equalTo(self.view)
+                make.top.equalTo(self.view)
+            }
+        }
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
