11c8ce2c2fa369ccce8e699243d6a3f40911c161
Jamie De Vivo (i7436295) <i7436295@mac42943.student.bournemouth.ac.uk>
2019-02-18 20:55:06 +0000

    Added Register functionality

--- me/RegisterViewController.swift
+++ me/RegisterViewController.swift
@@ -1,22 +1,69 @@
-//
-//  RegisterViewController.swift
-//  me
-//
-//  Created by Jamie De Vivo (i7436295) on 17/02/2019.
-//  Copyright © 2019 Jamie De Vivo. All rights reserved.
-//
-
 import UIKit
-import SnapKit
 import Firebase
+import SnapKit
 
 class RegisterViewController: UIViewController {
-
+    
+    let emailTextField = UITextField()
+    let passwordTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
 
+        let registerButton = UIButton()
+        
+        print("LOG: Login Screen")
+        
+        emailTextField.backgroundColor = .gray
+        emailTextField.text = "email"
+        passwordTextField.backgroundColor = .gray
+        passwordTextField.text = "password"
+        
+        registerButton.setTitle("Register", for: .normal)
+        registerButton.setTitleColor(.blue, for: .normal)
+        registerButton.addTarget(self, action: #selector(RegisterViewController.registerButtonAction), for: .touchUpInside)
+        
+        self.view.addSubview(emailTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(registerButton)
+        
+        emailTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(35)
+            make.centerX.centerY.equalTo(self.view)
+        }
+        passwordTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(35)
+            make.top.equalTo(emailTextField.snp.bottom).offset(20)
+        }
+        registerButton.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(40)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+        }
+        
     }
-
+    
+    @objc func registerButtonAction(_ sender: Any) {
+        
+        print("Register with details: \(emailTextField.text!)  \(passwordTextField.text!)")
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            print("1")
+            if let _ = authResult {
+                print("2")
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
 }
