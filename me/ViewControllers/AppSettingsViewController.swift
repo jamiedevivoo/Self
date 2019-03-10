303c9aafa834f8f777d67ba7e0eb9b4ef0294b22
Jamie De Vivo <jamiedevivoo@users.noreply.github.com>
2019-03-10 17:28:59 +0000

    Added profile and app settings pages

---
+++ me/ViewControllers/AppSettingsViewController.swift
@@ -0,0 +1,202 @@
+import UIKit
+import SnapKit
+import Firebase
+
+class AppSettingsViewController: UIViewController {
+    
+    
+    // MARK: - Properties
+    
+    var user: User?
+    
+    
+    // MARK: - SubViews
+    
+    lazy var topView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.app.background.primary
+        return view
+    }()
+    lazy var pageTipLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Use this page to modify your account and app settings."
+        label.textAlignment = .left
+        label.textColor = .darkText
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "First Name"
+        return textField
+    }()
+    lazy var surnameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Surname"
+        return textField
+    }()
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Email Address"
+        return textField
+    }()
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        addConstraints()
+        self.user = AccountManager.shared.user
+        updateFields()
+    }
+    
+    
+    // MARK: - Functions
+    
+    func setupView() {
+        title = "App Settings"
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItems = nil
+        
+        self.view.addSubview(topView)
+        topView.addSubview(pageTipLabel)
+        self.view.addSubview(nameTextField)
+        self.view.addSubview(surnameTextField)
+        self.view.addSubview(emailTextField)
+    }
+    
+    func updateFields() {
+        if let name = self.user?.name {
+            self.nameTextField.text = "\(name)"
+        }
+        if let surname = self.user?.surname {
+            self.surnameTextField.text = "\(surname)"
+        }
+        if let email = self.user?.email {
+            self.emailTextField.text = "\(email)"
+        }
+        
+        //        AccountManager.shared.update(user)
+    }
+    
+    // MARK: - Actions
+    
+    //        @objc func saveButtonAction(_ sender: Any) {
+    //
+    //            guard let name = nameTextField.text, let surname = surnameTextField.text, let email = emailTextField.text else { return }
+    //
+    //    //        let user = Auth.auth().currentUser
+    //    //        var credential: AuthCredential
+    //    //
+    //    //        user?.reauthenticate(with: credential) { error in
+    //    //            if let error = error {
+    //    //                // An error happened.
+    //    //            } else {
+    //    //                // User re-authenticated.
+    //    //            }
+    //    //        }
+    
+    
+    // MARK: - Functions
+    
+    
+    
+    //
+    ////        let user = User()
+    //
+    //
+    //        print("LOG: Account Settings Screen")
+    //
+    //        view.backgroundColor = .gray
+    //
+    //        let saveButton = UIButton()
+    //
+    
+    
+    
+    //
+    //        self.emailTextField.text = user?.email
+    //        self.nameTextField.text = user?.name
+    //        self.surnameTextField.text = user?.surname
+    //
+    //        saveButton.setTitle("Save Details", for: .normal)
+    //        saveButton.setTitleColor(.blue, for: .normal)
+    //        saveButton.addTarget(self, action: #selector(AccountSettingsViewController.saveButtonAction), for: .touchUpInside)
+    //
+    //        self.view.addSubview(saveButton)
+    //
+    
+    
+    
+    //        saveButton.snp.makeConstraints { (make) in
+    //            make.left.equalTo(100)
+    //            make.right.equalTo(-100)
+    //            make.height.equalTo(40)
+    //            make.top.equalTo(surnameTextField.snp.bottom).offset(30)
+    //        }
+    //
+    //    }
+    //
+    //
+    //
+    //        Auth.auth().currentUser?.updateEmail(to: email) { (error) in
+    //            if let _ = error {
+    //                print ("\(String(describing: error))")
+    //            } else {
+    //
+    //                let userData: [String:Any] = ["name": name,
+    //                                "surname ": surname,
+    //                                "email":email]
+    //                let uid:String = (AccountManager.shared.user?.uid)!
+    //
+    //                self.db.collection("user").document(uid).updateData(userData)
+    //
+    //                print("Updated Details: \(email) and \(name) and \(surname)")
+    //            }
+    //        }
+    //
+    //
+    //    }
+    
+}
+
+extension AppSettingsViewController: ConstraintBuilding {
+    func addConstraints() {
+        topView.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.height.equalTo(200)
+        }
+        pageTipLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(topView.snp.left).offset(20)
+            make.right.equalTo(topView.snp.right).offset(-20)
+            make.top.equalTo(topView.snp.top).offset(100)
+        }
+        nameTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(35)
+            make.top.equalTo(emailTextField.snp.bottom).offset(20)
+        }
+        
+        surnameTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(35)
+            make.top.equalTo(nameTextField.snp.bottom).offset(20)
+        }
+        
+        emailTextField.snp.makeConstraints { (make) in
+            make.left.equalTo(100)
+            make.right.equalTo(-100)
+            make.height.equalTo(35)
+            make.centerX.centerY.equalTo(self.view)
+        }
+    }
+}
