1664dc8daa89582233366c06776894d539e6bd63
Jamie De Vivo <accounts@jamiedevivo.com>
2019-05-07 16:02:32 +0100

    hiding emotions based on mood

--- App/Self/Model Controllers/EmotionManager.swift
+++ App/Self/Model Controllers/EmotionManager.swift
@@ -6,23 +6,24 @@ class EmotionManager {
         Mood.Emotion.init(name: "Anger", adj: "angry", valence: -0.8, arousal: 0.8),
         Mood.Emotion.init(name: "Boredom", adj: "bored", valence: 0.4, arousal: -0.8),
         Mood.Emotion.init(name: "Excitement", adj: "excited", valence: 0.8, arousal: 0.8),
-        Mood.Emotion.init(name: "Depression", adj: " ", valence: -0.8, arousal: -0.8),
+        Mood.Emotion.init(name: "Depression", adj: "Depressed", valence: -0.8, arousal: -0.8),
         Mood.Emotion.init(name: "Okay", adj: "okay", valence: 0, arousal: 0)
     ]
-    static let emotionsDBRef: CollectionReference = Firestore.firestore().collection("emotions")
+    let emotionsDBRef: CollectionReference = Firestore.firestore().collection("emotions")
     
-    static func getAllEmotions(completion: @escaping () -> ()) {
+    func getAllEmotions(completion: @escaping ([Mood.Emotion]) -> ()) {
         emotionsDBRef.getDocuments() { emotionCollection, error in
             guard let emotionCollection = emotionCollection, error == nil else {
                 if let error = error { print("Error Loading Emotions: \(error.localizedDescription)") }
                 return
             }
             print(emotionCollection)
+            var allEmotions: [Mood.Emotion] = []
             for emotionDocument in emotionCollection.documents {
                 let emotion = Mood.Emotion(emotionDocument.data())
-                self.allEmotions.append(emotion)
+                allEmotions.append(emotion)
             }
-            completion()
+            completion(allEmotions)
         }
     }
         
--- App/Self/View Controllers/Mood_LoggingMood-ViewController.swift
+++ App/Self/View Controllers/Mood_LoggingMood-ViewController.swift
@@ -63,9 +63,10 @@ final class MoodLoggingMoodViewController: ViewController {
     var isMoodMarked:Bool = false
     var markAdded: Bool = false
     
-    var arousalRating: Double?
-    var valenceRating: Double?
+    var userRatings: (valence: Double, arousal: Double) = (0,0)
     var emotion: Mood.Emotion?
+    
+    var emotionLabelCollection: [UILabel] = []
     
 }
 
@@ -78,10 +79,10 @@ extension MoodLoggingMoodViewController {
         setupChildViews()
         circle.addGestureRecognizer(tapGesture)
         screenSlider?.forwardNavigationEnabled = false
+        super.navigationController?.isNavigationBarHidden = true
+        navigationController?.isToolbarHidden = true
         
-        screenSlider?.forwardNavigationEnabled = true
-        screenSlider?.gestureSwipingEnabled = true
-        super.navigationController?.isNavigationBarHidden = false
+        addEmotionLabels()
     }
     
     
@@ -123,18 +124,29 @@ extension MoodLoggingMoodViewController {
     func updateMark(touch: UITouch) {
         
         let location = touch.location(in: self.view)
-        let moodRatings = calculateMood(x: location.x, y: location.y)
-        valenceRating = moodRatings["Valence"]
-        arousalRating = moodRatings["Arousal"]
-        emotion = EmotionManager.getEmotion(withValence: moodRatings["Valence"]!, withArousal: moodRatings["Arousal"]!)
+        
+        /// Create appropriate mood
+        userRatings = calculateMoodFromScreenPosition(
+            x: location.x,
+            y: location.y
+        )
+        emotion = EmotionManager.getEmotion(
+            withValence: userRatings.valence,
+            withArousal: userRatings.arousal
+        )
+        
+        /// - Update the labels
         emotionPickerLabel.text = emotion?.adj
         emotionPickerLabel.frame.origin.x = location.x - 50
         emotionPickerLabel.frame.origin.y = location.y - 100
+        
+        /// - Update tap to confirm and circle positions
         tapToConfirm.frame.origin.x = location.x - 50
         tapToConfirm.frame.origin.y = location.y + 20
-        circle.frame.origin.x = location.x - 25
-        circle.frame.origin.y = location.y - 25
+        circle.frame.origin.x = location.x - (circle.frame.width / 2)
+        circle.frame.origin.y = location.y - (circle.frame.height / 2)
         
+        updateLabelsRelativeToPosition(tapPosition: (location.x, location.y))
         updateBackground(xScale: (location.x / view.frame.width), yScale: (location.y / view.frame.height))
     }
     
@@ -155,10 +167,10 @@ extension MoodLoggingMoodViewController {
     
     func saveMarkedMood() {
         guard isMoodMarked == true else { return }
-        self.dataCollectionDelegate?.setData([
-            "arousalRating":arousalRating,
-            "valenceRating":valenceRating,
-            "emotion":emotion]
+        self.dataCollectionDelegate?.setData(
+            ["arousalRating" :userRatings.arousal,
+             "valenceRating" :userRatings.valence,
+             "emotion"       :emotion]
         )
         tapToConfirm.removeFromSuperview()
         screenSlider?.forwardNavigationEnabled = true
@@ -166,13 +178,12 @@ extension MoodLoggingMoodViewController {
         screenSlider?.nextScreen()
     }
     
-    func calculateMood(x: CGFloat, y: CGFloat) -> Dictionary<String, Double> {
+    func calculateMoodFromScreenPosition(x: CGFloat, y: CGFloat) -> (valence: Double, arousal: Double) {
         let arousalCoordinates = -convertCoordinateToRating(coordinate: y, range: self.view.frame.height)
         let valenceCoordinates = convertCoordinateToRating(coordinate: x, range: self.view.frame.width)
         let arousal:Double = Double(arousalCoordinates)
         let valence:Double = Double(valenceCoordinates)
-        let mood = ["Valence":valence,"Arousal":arousal]
-        return mood
+        return (valence: valence, arousal: arousal)
     }
     
     func convertCoordinateToRating(coordinate: CGFloat, range: CGFloat) -> CGFloat {
@@ -188,10 +199,76 @@ extension MoodLoggingMoodViewController {
         let blue    = 0.1 + (0.8 * yScale)   // 0.1 - 0.9
         let alpha   = 0.6 - (0.2 * yScale)   // 0.6 - 0.4
         
-        print(red, green, blue, alpha)
-        
         view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: alpha)
 
+    }
+    
+    func addEmotionLabels() {
+        for emotion in EmotionManager.allEmotions {
+            let valenceMultiplier = emotion.valence + 1
+            let arousalMultiplier = emotion.arousal + 1
+            
+            /// - Create and style the label
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: 10, weight: UIFont.Weight.ultraLight)
+            label.textAlignment = .center
+            label.layer.frame.size = CGSize(width: 80, height: 20)
+            
+            /// - Add the emotion name
+            label.text = emotion.name
+            
+            /// - Position the label
+            let left: CGFloat = CGFloat(self.view.frame.width / 2) - (label.frame.width / 2)    /// First: Get the center horizontal position of the view and label
+            label.frame.origin.x = left*CGFloat(valenceMultiplier)                              /// Second: Adjust that position by the multiplier
+            let top: CGFloat = CGFloat(self.view.frame.height / 2) - (label.frame.height / 2)   /// First: Get the center vertical position of the view and label
+            label.frame.origin.y = top*CGFloat(arousalMultiplier)                               /// Second: Adjust that position by the multiplier
+            
+            /// - Add the label
+            view.addSubview(label)
+            emotionLabelCollection.append(label)
+        }
+    }
+    
+    func updateLabelsRelativeToPosition(tapPosition tap:(x: CGFloat, y: CGFloat)) {
+        
+        for emotionLabel in emotionLabelCollection {
+            
+            /// - Calculate the frame radius now (axis size divide by 2), to help keep the code clean later on.
+            let radius: (width:CGFloat,height:CGFloat) = (
+                    width: emotionLabel.frame.width / 2,
+                    height: emotionLabel.frame.height / 2
+            )
+            
+            /// - Calculate the position of the center of the label
+            let position: (x:CGFloat,y:CGFloat) = (
+                x: emotionLabel.frame.origin.x + radius.width,
+                y: emotionLabel.frame.origin.y + radius.height
+            )
+            
+            /// - Work out how close the user is to this emotion
+            let distanceFromTap = (
+                x: abs(position.x - tap.x),
+                y: abs(position.y - tap.y),
+                total: abs(position.x - tap.x) + abs(position.y - tap.y)
+            )
+            
+            let fractionDistanceFromTap = distanceFromTap.total / 2
+            
+//            let xDistanceFraction     = xDistanceFromEmotion / (self.view.frame.width / 2)      /// Second: Convert it into a fraction between 0 (very close) and 1 (very far)
+//            let yDistanceFromEmotion  = abs(anchorPositon.y - y)                           /// First: Calculate the absolute distance from the labels vertical Anchor
+//            let yDistanceFraction     = yDistanceFromEmotion / (self.view.frame.height / 2)     /// Second: Convert it into a fraction between 0 (very close) and 1 (very far)
+//            let distanceFraction      = (xDistanceFraction + yDistanceFraction) / 2             /// Then: Combine them to have an overall fraction representing distance
+//
+//            /// - Create a Multiplier
+            var multiplier = fractionDistanceFromTap       /// First: Reverse the distanceFraction so that 0 now represents the furthest distance possible and 1 represents the closest
+            multiplier     = 1 - (multiplier / 3)  /// Second:
+            
+            
+//            multiplier     = multiplier +
+            
+            emotionLabel.alpha          = multiplier
+        }
+        
     }
     
 }
